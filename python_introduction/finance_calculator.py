3ab97af6b592c243489c20fc797ca3dd4d6e22eb
kerolosnady2 <kerolosnady777@gmail.com>
2025-05-25 23:24:44 +0300

    add file that calculate the saving salary per year

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -2,7 +2,7 @@ x = input("Enter your monthly income: ")
 input_one = int(x)
 y = input("Enter your total monthly expenses: ")
 input_two = int(y)
-monthly_saving = input_one - input_two
-print("Your monthly savings are $",monthly_saving)
-projected_savings = monthly_saving * 12 + (monthly_saving * 12 * 0.05)
+monthly_income = input_one - input_two
+print("Your monthly savings are $",monthly_income)
+projected_savings = monthly_income * 12 + (monthly_income * 12 * 0.05)
 print("Projected savings after one year, with interest, is: $",int(projected_savings))
