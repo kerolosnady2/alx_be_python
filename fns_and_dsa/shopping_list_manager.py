6d59a10a50849df2491c0e2127de8e5b2e9b342a
kerolosnady2 <kerolosnady777@gmail.com>
2025-06-08 18:08:14 +0300

    Handle shopping list system logic

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,7 +1,5 @@
-# shopping_list_manager.py
-
 def display_menu():
-    print("\nShopping List Manager")
+    print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
@@ -29,7 +27,7 @@ def main():
                 print(f'"{item}" not found in your shopping list.')
         elif choice == '3':
             if shopping_list:
-                print("\nYour Shopping List:")
+                print("Your Shopping List:")
                 for i, item in enumerate(shopping_list, start=1):
                     print(f"{i}. {item}")
             else:
