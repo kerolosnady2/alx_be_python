79784a92befbf09ef7d2d586736169458765c516
kerolosnady2 <kerolosnady777@gmail.com>
2025-06-08 17:55:01 +0300

    Simple calculator

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,16 +1,15 @@
 def perform_operation(num1, num2, operation):
     match operation:
         case 'add':
-            result = num1 + num2
+            return num1 + num2
         case 'subtract':
-            result = num1 - num2
+            return num1 - num2
         case 'multiply':
-            result = num1 * num2
+            return num1 * num2
         case 'divide':
             if num2 == 0:
                 print("The divisor cann't be zero, ERROR!")
-            else:
-                result = num1 / num2
+            elif num2 != 0:
+                return num1 / num2
         case _:
             print("Not valid operation.")
-    return result
